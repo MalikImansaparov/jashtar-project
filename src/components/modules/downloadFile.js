66f6c378c637ae24e45acb9929a96ddb08c38532
imansaparov <malikimansaparov@gmail.com>
2022-11-05 22:17:55 +0600

    fix

--- src/components/modules/downloadFile.js
+++ src/components/modules/downloadFile.js
@@ -1,19 +1,23 @@
 
 
 export const downloadFile = async (url) => {
-    fetch(`https://jashtar.info${url}`, {
-        headers: {
-            accept: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        },
-    })
-        .then((response) => response.blob())
-        .then((blob) => {
-            const url = window.URL.createObjectURL(new Blob([blob]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = "a.docx";
-            document.body.appendChild(link);
-            link.click();
-            link.parentNode.removeChild(link);
-        });
+     const name = url.toString();
+     const arr = name.split('/');
+     const filename = arr[arr.length - 1];
+     fetch(`https://jashtar.info${url}`, {
+       headers: {
+         accept:
+           'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+       },
+     })
+       .then((response) => response.blob())
+       .then((blob) => {
+         const url = window.URL.createObjectURL(new Blob([blob]));
+         const link = document.createElement('a');
+         link.href = url;
+         link.download = filename;
+         document.body.appendChild(link);
+         link.click();
+         link.parentNode.removeChild(link);
+       });
 }
--- src/pages/documentPage/DocChldren/categoryDetail.js
+++ src/pages/documentPage/DocChldren/categoryDetail.js
@@ -47,72 +47,113 @@ const DocumentDetail = () => {
 
 
     return (
-        <div className='wrapper text-justify'>
-            <div className="m-auto mb-[62px] max-w-[1220px] mt-16">
-                {response &&
-                    <>
-                        {i18n.language === "ky" &&
-                    <>
-                        <BreadCrumbs crumbs={crumbs} title={response.title_ky} />
-                        <div className=" flex flex-wrap">
-                         <div dangerouslySetInnerHTML={createMarkupDoc(response.body_ky)}></div>
-                        </div>
-                        <div className="sm:mx-4">
-                        <p className="text-base font-medium text-blue mb-[28px]">{t('download')}</p>
-                        <div className="max-w-[400px]">
-                            <p className="text-[13px] font-normal mb-3 ">{response.doc.map(item => item.file_name)}</p>
-                        </div>
-                        <div className='flex '>
-                            <div className='w-[152px] cursor-pointer ' onClick={() => downloadFile(response.doc.map(item => item.file))}>
-                                <img src={doc} alt='pdf'/>
-                                <img src={download} alt='pdf' className="ml-3"/>
-                            </div>
-                        </div>
-            </div>
-                    </>
-                    }
-                        {i18n.language === "en" &&
-                            <>
-                                <BreadCrumbs crumbs={crumbs} title={response.title_en} />
-                                <div className="flex text-center justify-center items-center">
-                                    <div dangerouslySetInnerHTML={createMarkup(response.body_en)}></div>
-                                </div>
-                                <p className="text-base font-medium text-blue mb-[28px]">{t('download')}</p>
-                                <div className="w-[400px]">
-                                    <p className="text-[13px] font-normal mb-3 ">{response.doc.map(item => item.file_name)}</p>
-                                </div>
-                                <div className='flex'>
-                                    <div className='w-[152px] cursor-pointer' onClick={() => downloadFile(response.doc.map(item => item.file))}>
-                                        <img src={doc} alt='pdf'/>
-                                        <img src={download} alt='pdf' className="ml-3"/>
-                                    </div>
-                                </div>
-                            </>
+      <div className="wrapper text-justify">
+        <div className="m-auto mb-[62px] max-w-[1220px] mt-16">
+          {response && (
+            <>
+              {i18n.language === 'ky' && (
+                <>
+                  <BreadCrumbs crumbs={crumbs} title={response.title_ky} />
+                  <div className=" flex flex-wrap">
+                    <div
+                      dangerouslySetInnerHTML={createMarkupDoc(
+                        response.body_ky
+                      )}
+                    ></div>
+                  </div>
+                  <div className="sm:mx-4">
+                    <p className="text-base font-medium text-blue mb-[28px]">
+                      {t('download')}
+                    </p>
+                    <div className="max-w-[400px]">
+                      <p className="text-[13px] font-normal mb-3 ">
+                        {response.doc.map((item) => item.file_name)}
+                      </p>
+                    </div>
+                    <div className="flex ">
+                      <div
+                        className="w-[152px] cursor-pointer "
+                        onClick={() =>
+                          downloadFile(response.doc.map((item) => item.file))
                         }
-                        {i18n.language === "ru" &&
-                            <>
-                                <BreadCrumbs crumbs={crumbs} title={response.title_ru} />
-                                <div className="flex text-center justify-center items-center">
-                                    <div dangerouslySetInnerHTML={createMarkup(response.body_ru)}></div>
-                                </div>
-                                <p className="text-base font-medium text-blue mb-[28px]">{t('download')}</p>
-                                <div className='flex'>
-                                    <div className='w-[152px] cursor-pointer'>
-                                        <img src={pdf} alt='pdf' mb-3/>
-                                        <img src={download} alt='pdf' onClick={() => downloadFile(response.doc.map(item => item))}/>
-                                    </div>
-                                    <div className='w-[152px] cursor-pointer'>
-                                        <img src={doc} alt='pdf'/>
-                                        <img src={download} alt='pdf' onClick={() => downloadFile(response.doc.map(item => item))}/>
-                                    </div>
-                                    <div className="text-[13px] font-normal ml-6">{response.doc.map(item => item.file_name)}</div>
-                                </div>
-                            </>
+                      >
+                        <img src={doc} alt="pdf" />
+                        <img src={download} alt="pdf" className="ml-3" />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+              {i18n.language === 'en' && (
+                <>
+                  <BreadCrumbs crumbs={crumbs} title={response.title_en} />
+                  <div className=" flex flex-wrap">
+                    <div
+                      dangerouslySetInnerHTML={createMarkupDoc(
+                        response.body_en
+                      )}
+                    ></div>
+                  </div>
+                  <div className="sm:mx-4">
+                    <p className="text-base font-medium text-blue mb-[28px]">
+                      {t('download')}
+                    </p>
+                    <div className="max-w-[400px]">
+                      <p className="text-[13px] font-normal mb-3 ">
+                        {response.doc.map((item) => item.file_name)}
+                      </p>
+                    </div>
+                    <div className="flex ">
+                      <div
+                        className="w-[152px] cursor-pointer "
+                        onClick={() =>
+                          downloadFile(response.doc.map((item) => item.file))
                         }
-                    </>
-                }
-            </div>
+                      >
+                        <img src={doc} alt="pdf" />
+                        <img src={download} alt="pdf" className="ml-3" />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+              {i18n.language === 'ru' && (
+                <>
+                  <BreadCrumbs crumbs={crumbs} title={response.title_en} />
+                  <div className=" flex flex-wrap">
+                    <div
+                      dangerouslySetInnerHTML={createMarkupDoc(
+                        response.body_en
+                      )}
+                    ></div>
+                  </div>
+                  <div className="sm:mx-4">
+                    <p className="text-base font-medium text-blue mb-[28px]">
+                      {t('download')}
+                    </p>
+                    <div className="max-w-[400px]">
+                      <p className="text-[13px] font-normal mb-3 ">
+                        {response.doc.map((item) => item.file_name)}
+                      </p>
+                    </div>
+                    <div className="flex ">
+                      <div
+                        className="w-[152px] cursor-pointer "
+                        onClick={() =>
+                          downloadFile(response.doc.map((item) => item.file))
+                        }
+                      >
+                        <img src={doc} alt="pdf" />
+                        <img src={download} alt="pdf" className="ml-3" />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
+          )}
         </div>
+      </div>
     );
 };
 
