0d951da867920b8b530fdebb6eaa176be33b4683
imansaparov <malikimansaparov@gmail.com>
2022-12-20 18:28:37 +0600

    update

--- src/components/main/FormPage/VolunterForm.js
+++ src/components/main/FormPage/VolunterForm.js
@@ -32,7 +32,7 @@ export const VolunterForm = () => {
               backgroundImage: `url(${uri}${response.background_image})`,
               backgroundRepeat: 'no-repeat',
               backgroundSize: 'cover',
-              backgroundPosition: 'center',
+              backgroundPosition: 'top',
             }}
           >
             <div className="absolute top-0 left-0 right-0 bg-gradient-ministry w-full min-h-[648px] xs:min-h-[664px] z-0"></div>
